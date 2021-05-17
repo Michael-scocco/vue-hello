f44c7cc46a0da24e46763d41840ca70cf9a55785
Michael Scocco <scocco.michael87@gmail.com>
2021-05-17 16:22:53 +0200

    step-2

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// vue-hello
+// Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+
+
+// Descrizione: Stampare a schermo un messaggio 
+// all’interno di un h1, utilizzando i data.
+
+let screen = new Vue(
+    {
+     el: '#app',
+     data:{
+         title: 'Learn vueJS',
+         vueTest: './img/vueTest.jpg'
+     }
+    });
+
+// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
